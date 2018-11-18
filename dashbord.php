8d3e2d9a370004ac0e6cdbd3965f2092f922f11a
Dinith Chanuka <30471992+dinithchanuka@users.noreply.github.com>
2018-11-18 21:37:53 +0530

    dashbord - File uploading sucess

--- dashbord.php
+++ dashbord.php
@@ -1,12 +1,12 @@
 
 <?php
-   if(isset($_FILES['image'])){
+   if(isset($_FILES['file'])){
       $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      $file_name = $_FILES['file']['name'];
+      $file_size = $_FILES['file']['size'];
+      $file_tmp = $_FILES['file']['tmp_name'];
+      $file_type = $_FILES['file']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
       
       $expensions= array("pdf","doc");
       
@@ -19,7 +19,7 @@
       }
       
       if(empty($errors)==true) {
-         move_uploaded_file($file_tmp,"images/".$file_name);
+         move_uploaded_file($file_tmp,"files/".$file_name);
          echo "Success";
       }else{
          print_r($errors);
